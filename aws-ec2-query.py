b86f559af0e007326b4a3ab80767bdf089e2ea78
William Brady <will@crofton.cloud>
2020-05-29 01:21:41 -0400

    syncing variable case

--- aws-ec2-query.py
+++ aws-ec2-query.py
@@ -10,17 +10,25 @@ S3_BUCKET = 'portfolio-918573727633-us-east-1-dataset'
 s3 = boto3.client('s3')
 
 r = s3.select_object_content(
-    Bucket=S3_BUCKET,
-    Key='dataset-numbered.csv',
-    ExpressionType='SQL',
-    Expression=S3_QUERY,
-    InputSerialization={'CSV': {"FileHeaderInfo": "Use"}},
-    OutputSerialization={'JSON': {}},
+  Bucket=S3_BUCKET,
+  Key='indexed_dataset.csv',
+  ExpressionType='SQL',
+  Expression=S3_QUERY,
+  InputSerialization={'CSV': {"FileHeaderInfo": "Use"}},
+  OutputSerialization={'JSON': {}},
 )
 
 for event in r['Payload']:
-    if 'Records' in event:
-        records = event['Records']['Payload'].decode('utf-8')
-        print(records)
-    elif
-        print("Record not found")
+  if 'Records' in event:
+    records = event['Records']['Payload'].decode('utf-8')
+    print(records)
+  elif 'Stats' in event:
+    statsDetails = event['Stats']['Details']
+    print("Stats details bytesScanned: ")
+    print(statsDetails['BytesScanned'])
+    print("Stats details bytesProcessed: ")
+    print(statsDetails['BytesProcessed'])
+    print("Stats details bytesReturned: ")
+    print(statsDetails['BytesReturned'])
+  else:
+   print("Record not found")
