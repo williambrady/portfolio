87b6bace7ecabb72f266272112b9c70a74f98e21
William Brady <will@crofton.cloud>
2020-05-28 22:38:59 -0400

    added file uploads to provisioning

--- aws-s3-dataset.tf
+++ aws-s3-dataset.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "dataset" {
+  depends_on = ["aws_s3_bucket.dataset"]
   bucket =  "${var.bucket_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
 
   acl           = "private"
@@ -23,4 +24,20 @@ resource "aws_s3_bucket_public_access_block" "dataset" {
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
+}
+# Upload indexed_dataset.csv to the bucket for accessibility.
+resource "aws_s3_bucket_object" "dataset" {
+  depends_on = ["aws_s3_bucket.dataset"]
+  bucket = "${aws_s3_bucket.dataset.id}"
+  key    = "indexed_dataset.csv"
+  source = "./indexed_dataset.csv"
+  tags = "${merge(map("Name","indexed_dataset.csv"), var.tags)}"
+}
+# Upload the data handler for the ec2-query.py for testing.
+resource "aws_s3_bucket_object" "ec2_query" {
+  depends_on = ["aws_s3_bucket.dataset"]
+  bucket = "${aws_s3_bucket.dataset.id}"
+  key    = "aws-ec2-query.py"
+  source = "./aws-ec2-query.py"
+  tags = "${merge(map("Name","aws-ec2-query.py"), var.tags)}"
 }
