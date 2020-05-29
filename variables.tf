2a43096b470ef7965af0f74c011ab78243993a2d
William Brady <will@crofton.cloud>
2020-05-29 00:10:07 -0400

    cleaned up global variables

--- variables.tf
+++ variables.tf
@@ -1,21 +1,23 @@
-variable "aws_account_id" {}
-variable "aws_profile" {}
-variable "aws_region" {}
-variable "project_name" {}
-variable "ec2_keyname" {}
-variable "mgmt_ip" {}
-
+variable "aws_account_id" { description = "AWS Account Number" }
+variable "aws_profile" { description = "Profile name in ~/.aws/credentials used to access target account" }
+variable "aws_region" { description = "default region to build in" }
 variable "bucket_prefix" {
+  description = "bucket prefix will brand assets"
   default = "portfolio"
 }
-
-variable "vpc_cidr" {}
+variable "ec2_keyname" { description = "EC2 SSH Key Name" }
+variable "mgmt_ip" { description = "IP Address of the deployment system, required for debugging" }
+variable "bucket_key_prefix" {
+  description = ""
+  default = "portfolio-config"
+}
+variable "vpc_cidr" { description = "segmented network" }
 variable "vpc_subnet_pub_a" {}
 variable "vpc_subnet_pub_b" {}
 variable "vpc_subnet_priv_a" {}
 variable "vpc_subnet_priv_b" {}
-
 variable "tags" {
+  description = "set default tags for assets"
   default = {
     "owner"   = "william.brady"
     "lob"     = "cloudops"
