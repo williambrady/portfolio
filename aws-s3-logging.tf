c194f9b26f72a44c114fd0ea01d03fa4cebdfa7f
William Brady <will@crofton.cloud>
2020-05-30 16:53:23 -0400

    added depends_on for policy applications

--- aws-s3-logging.tf
+++ aws-s3-logging.tf
@@ -97,4 +97,5 @@ resource "aws_s3_bucket_public_access_block" "logging" {
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
+  depends_on = ["aws_s3_bucket_policy.logging"]
 }
