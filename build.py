84164c750bfa8f4166813eab4f53d23b8a0e1a70
William Brady <will@crofton.cloud>
2020-05-31 20:54:26 -0400

    added lambda packager

--- build.py
+++ build.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-#
-# Add line numbers to a CSV to emulate a database index.
-###
+"""
+Open the initial CSV file and add row numbers to emulate an index.
+This will allow AWS S3 Select to query the file directly instead of requiring a database.
+This loader will also package the lambda function for inclusion in the S3 upload.
+"""
+import subprocess
+
 sourcefile = 'dataset.csv'
 destfile = 'indexed_dataset.csv'
 log = open(destfile,"w")
 
+print("Creating an indexed version of the original data.")
 with open(sourcefile) as fp:
     line = fp.readline()
     cnt = 0
     while line:
         if cnt == 0 :
-            log.write ("Id,{}\n".format(line.strip()))
-            print ("Id,{}".format(line.strip()))
+            log.write ("carId,{}\n".format(line.strip()))
+            # print ("carId,{}".format(line.strip()))
         else:
             log.write ("{},{}\n".format(cnt, line.strip()))
-            print ("{},{}".format(cnt, line.strip()))
+            # print ("{},{}".format(cnt, line.strip()))
         line = fp.readline()
         cnt += 1
 
 log.close()
-print("\nOutput file {} created.".format(destfile))
+print("Output file {} created.".format(destfile))
 
-# Let's verify a valid SSH key is in place.
-# grep terraform vars for ec2_keyname then verify it exists in ~/.ssh
-# if not, ssh-keygen  -b 4096 and bake it in
+"""
+Lambda Packager: zip the python file into a zip with no path included.
+This is required for successful Lambda inclusion.
+"""
+
+ZIP = subprocess.check_output("zip -j aws-lambda-portfolio-payload.zip lambda_function.py",shell=True).strip()
+ZIP = ZIP.decode().strip()
+print("Zipping lambda function: {}".format(ZIP))
+print("") # Adding a trailing \n for asthetics
