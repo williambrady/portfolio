342b9c31d975d9b2a74c670f9ba2f7796dc2bba3
William Brady <will@crofton.cloud>
2020-05-31 20:56:51 -0400

    initial commit

---
+++ test-api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Test the API to ensure results are as expected.
+The initial data set should return 5 successful responses,
+but the API needs to properly handle invalid ID's as well.
+Counting from 1 - 10 should give a proper sampling.
+"""
+import subprocess
+import requests
+
+# Get the API Endpoint from terraform output
+URL = subprocess.check_output("terraform output aws_api_endpoint",shell=True).strip()
+
+# Walk through the values and return the API responses for each.
+i = 0
+while i < 10:
+  response = requests.get(URL, params={"carId":i})
+  print("Requesting carID {}",i)
+  print(response.json())
+  print("")
+  i += 1
