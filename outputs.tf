fcd1097adbe4443d87e4f1a054eebc692cc15786
William Brady <will@crofton.cloud>
2020-05-31 20:56:08 -0400

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,11 @@
-# S3 Info
-output "aws_s3_bucket_logging" {
-  value = "${aws_s3_bucket.logging.id}"
-}
 output "aws_s3_bucket_dataset" {
   value = "${aws_s3_bucket.dataset.id}"
 }
 
-# CloudTrail Info
-output "aws_cloudtrail_portfolio" {
-  value = "${"aws_cloudtrail.portfolio.id"}"
-}
-
-# IAM Info
-output "aws_iam_instance_profile" {
-  value = "${aws_iam_instance_profile.s3read.arn}"
-}
 output "aws_iam_lambda_role" {
   value = "${aws_iam_role.lambda_s3_read.arn}"
 }
 
-# Bastion host information
-output "aws_bastion_public_ip" {
-  value = "${aws_instance.bastion.public_ip}"
-}
-output "aws_bastion_iam_instance_profile" {
-  value = "${aws_instance.bastion.iam_instance_profile}"
-}
-output "aws_bastion_keyname" {
-  value = "${var.ec2_keyname}"
-}
-# VPC Info
-output "aws_vpc_cidr"{
-  value = "${aws_vpc.main.cidr_block}"
-}
-output "aws_vpc_public_subnet_a"{
-  value = "${aws_subnet.public-subnet-a.cidr_block}, ${aws_subnet.public-subnet-a.availability_zone}"
-}
-output "aws_vpc_public_subnet_b"{
-  value = "${aws_subnet.public-subnet-b.cidr_block}, ${aws_subnet.public-subnet-b.availability_zone}"
-}
-output "aws_vpc_private_subnet_a"{
-  value = "${aws_subnet.private-subnet-a.cidr_block}, ${aws_subnet.private-subnet-a.availability_zone}"
-}
-output "aws_vpc_private_subnet_b"{
-  value = "${aws_subnet.private-subnet-b.cidr_block}, ${aws_subnet.private-subnet-b.availability_zone}"
-}
-# API gateway
-output "api_gateway" {
+output "aws_api_endpoint" {
   value = "${aws_api_gateway_deployment.portfolio.invoke_url}"
 }
