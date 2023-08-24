eeb336ece09cfd0633327624cdc7ecd7007d82ac
william brady <will@crofton.cloud>
2023-08-24 15:58:28 -0400

    testing

--- aws-s3-dataset.tf
+++ aws-s3-dataset.tf
@@ -1,20 +1,11 @@
 resource "aws_s3_bucket" "dataset" {
-  depends_on = [aws_s3_bucket.dataset]
+  depends_on = [aws_s3_bucket.logging]
   bucket     = "${var.project_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
-
-  # acl           = "private"
-  # region        = var.aws_region
   force_destroy = true
-
   logging {
     target_bucket = aws_s3_bucket.logging.id
     target_prefix = "s3-logs/${var.project_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
   }
-
-  # versioning {
-  #   enabled = true
-  # }
-
   provisioner "local-exec" {
     command = "python3 build.py ${var.infile}"
   }
@@ -38,14 +29,6 @@ resource "aws_s3_bucket_public_access_block" "dataset" {
 }
 
 # Upload indexed_dataset.csv to the bucket for accessibility.
-# resource "aws_s3_bucket_object" "dataset" {
-#   depends_on = [aws_s3_bucket.dataset]
-#   bucket     = aws_s3_bucket.dataset.id
-#   key        = "indexed_dataset.csv"
-#   source     = "./indexed_dataset.csv"
-#   tags       = var.tags
-# }
-
 resource "aws_s3_object" "dataset" {
   depends_on = [aws_s3_bucket.dataset]
   bucket = aws_s3_bucket.dataset.id
@@ -54,14 +37,6 @@ resource "aws_s3_object" "dataset" {
 }
 
 # Upload the lambda function.
-# resource "aws_s3_bucket_object" "lambda_function" {
-#   depends_on = [aws_s3_bucket.dataset]
-#   bucket     = aws_s3_bucket.dataset.id
-#   key        = "lambda_function.zip"
-#   source     = "./lambda_function.zip"
-#   tags       = var.tags
-# }
-
 resource "aws_s3_object" "lambda_function" {
   depends_on  = [aws_s3_bucket.dataset]
   bucket      = aws_s3_bucket.dataset.id
@@ -71,14 +46,6 @@ resource "aws_s3_object" "lambda_function" {
 }
 
 # Upload original dataset.csv to the bucket for accessibility.
-# resource "aws_s3_bucket_object" "og_dataset" {
-#   depends_on = [aws_s3_bucket.dataset]
-#   bucket     = aws_s3_bucket.dataset.id
-#   key        = var.infile
-#   source     = "./${var.infile}"
-#   tags       = var.tags
-# }
-
 resource "aws_s3_object" "og_dataset" {
   depends_on  = [aws_s3_bucket.dataset]
   bucket      = aws_s3_bucket.dataset.id
