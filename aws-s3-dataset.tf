8f777b1aba999d36db96b91a3969b097ffc58dbc
William Brady <will@crofton.cloud>
2023-09-09 01:59:11 -0400

    feat-002: automating deployment

--- aws-s3-dataset.tf
+++ aws-s3-dataset.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "dataset" {
-  depends_on = [aws_s3_bucket.logging]
-  bucket     = "${var.project_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
+  depends_on    = [aws_s3_bucket.logging]
+  bucket        = "${var.project_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
   force_destroy = true
   # logging {
   #   target_bucket = aws_s3_bucket.logging.id
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "dataset" {
 }
 
 resource "aws_s3_bucket_logging" "dataset" {
-  bucket = aws_s3_bucket.dataset.id
+  bucket        = aws_s3_bucket.dataset.id
   target_bucket = aws_s3_bucket.logging.id
   target_prefix = "s3-logs/${var.project_prefix}-${var.aws_account_id}-${var.aws_region}-dataset"
 }
@@ -37,25 +37,25 @@ resource "aws_s3_bucket_public_access_block" "dataset" {
 # Upload indexed_dataset.csv to the bucket for accessibility.
 resource "aws_s3_object" "dataset" {
   depends_on = [aws_s3_bucket.dataset]
-  bucket = aws_s3_bucket.dataset.id
-  key    = "indexed_dataset.csv"
-  source = "./indexed_dataset.csv"
+  bucket     = aws_s3_bucket.dataset.id
+  key        = "indexed_dataset.csv"
+  source     = "./indexed_dataset.csv"
 }
 
 # Upload the lambda function.
 resource "aws_s3_object" "lambda_function" {
-  depends_on  = [aws_s3_bucket.dataset]
-  bucket      = aws_s3_bucket.dataset.id
-  key         = "lambda_function.zip"
-  source      = "./lambda_function.zip"
-  tags        = var.tags
+  depends_on = [aws_s3_bucket.dataset]
+  bucket     = aws_s3_bucket.dataset.id
+  key        = "lambda_function.zip"
+  source     = "./lambda_function.zip"
+  tags       = var.tags
 }
 
 # Upload original dataset.csv to the bucket for accessibility.
 resource "aws_s3_object" "og_dataset" {
-  depends_on  = [aws_s3_bucket.dataset]
-  bucket      = aws_s3_bucket.dataset.id
-  key         = var.infile
-  source      = "./${var.infile}"
-  tags        = var.tags
+  depends_on = [aws_s3_bucket.dataset]
+  bucket     = aws_s3_bucket.dataset.id
+  key        = var.infile
+  source     = "./${var.infile}"
+  tags       = var.tags
 }
--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
   # profile = var.aws_profile
 }
 
 terraform {
   backend "s3" {
-    bucket = "918573727633-portfolio-state"
-    key    = "portfolio.terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
+    bucket     = "918573727633-portfolio-state"
+    key        = "portfolio.terraform.tfstate"
+    region     = "us-east-1"
+    encrypt    = true
     kms_key_id = "arn:aws:kms:us-east-1:918573727633:key/710cafd7-3970-4454-9d12-b72b7141d577"
   }
 }
