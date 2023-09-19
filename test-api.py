75349b1c648aed2ad8182b1f4c7d4c00d030e4ee
William Brady <will@crofton.cloud>
2023-09-19 09:11:35 -0400

    updated to python3

--- test-api.py
+++ test-api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Test the API to ensure results are as expected.
 The initial data set should return 5 successful responses,
@@ -9,8 +9,11 @@ import subprocess
 import requests
 
 # Get the API Endpoint from terraform output
-URL = subprocess.check_output("terraform output aws_api_endpoint",shell=True).strip()
+# Remove spaces with strip, remove quotes with replace, and convert to string.
+TF_URL = subprocess.check_output("terraform output aws_api_endpoint",shell=True).strip()
+URL = TF_URL.decode("utf-8").replace('"','')
 
+print("API Endpoint: %s" % URL)
 # Walk through the values and return the API responses for each.
 i = 0
 while i < 10:
